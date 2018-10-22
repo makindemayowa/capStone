78edf2854423a86dc2a8bb9935c77916f23f1387
Mayowa Makinde <mankinde53@yahoo.com>
2018-10-22 20:32:33 +0100

    setup server route for search

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,11 +19,36 @@ module.exports = (app) => {
         });
       })
       .catch((err) => {
-        console.log(err.message)
         return res.status(400).json({
           message: 'error', err: err.message
         });
       })
     }
   );
+
+  app.get('/api/search',
+  (req, res) => {
+    const q = req.query.q;
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=bAugSybKD0aQdl01yzWb1hQA535v0c6b&q=${q}&limit=10`)
+    .then((resp) => {
+      const gifArr = resp.data.data;
+      const result = []
+      gifArr.forEach(gif => {
+        const newGif = {
+          title: gif.title.split(" ").join(""),
+          imgUrl: gif.images.original.url
+        }
+        result.push(newGif);
+      });
+      return res.status(200).json({
+        message: 'success', result
+      });
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        message: 'error', err: err.message
+      });
+    })
+  }
+);
 }
