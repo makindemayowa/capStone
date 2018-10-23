a76e2b31c1e7ebdd1f81f3afdb2b77223cff77c9
Mayowa Makinde <mankinde53@yahoo.com>
2018-10-23 17:33:33 +0100

    setup route for user auth

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  bcrypt = require('bcrypt-nodejs'),
+  SALT_WORK_FACTOR = 10;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true
+  },
+  password: String,
+},
+{ timestamps: { createdAt: 'created_at' } });
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+    user.password = bcrypt.hashSync(user.password, salt);
+    next();
+  });
+});
+
+UserSchema.methods.comparePassword = function (plainText) {
+    return bcrypt.compareSync(plainText, this.password);
+  },
+
+module.exports = mongoose.model('User', UserSchema);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const axios = require("axios");
-var FormData = require('form-data');
-var qs = require('qs')
+const User = require('../models/user');
 
+const secret = process.env.JWT_SECRET_KEY;
+
+const jwt = require('jsonwebtoken');
+
+const createToken = (user, expiryTime) => jwt.sign(user, secret, { expiresIn: expiryTime })
 // twitter app 
 // https://stackoverflow.com/questions/45078952/twitter-api-application-only-authentication
 module.exports = (app) => {
@@ -57,33 +61,88 @@ module.exports = (app) => {
   );
 
   app.get('/api/trends',
-  (req, res) => {
-    axios.defaults.headers.common.Authorization = `Bearer AAAAAAAAAAAAAAAAAAAAAF7w8gAAAAAAmZWpnq5YUDGyc1k%2FaCH3pN320jM%3DEuUjOjdtjwkw32RaeruZlNLVbHDifmQgAIL2AJSFvbZr4kpf7O`;
-    axios.get(`https://api.twitter.com/1.1/trends/place.json?id=1398823`)
-      .then((resp) => {
-        const trending = resp.data[0].trends;
-        const trends = [];
-        trending.forEach(trend => {
-          const newTrend = {
-            title: trend.name.split(" ").join(""),
-          }
-          if(newTrend.title.charAt(0) !== '#') {
-            newTrend.query = newTrend.title.split(/(?=[A-Z])/).join(" ")
-            newTrend.title = `#${newTrend.title}`;
-          } else {
-            newTrend.query = newTrend.title.substr(1).split(/(?=[A-Z])/).join(" ")
-          }
-          trends.push(newTrend);
-        });
-        return res.status(200).json({
-          message: 'success', trends
-        });
-      })
-      .catch((err) => {
+    (req, res) => {
+      axios.defaults.headers.common.Authorization = `Bearer AAAAAAAAAAAAAAAAAAAAAF7w8gAAAAAAmZWpnq5YUDGyc1k%2FaCH3pN320jM%3DEuUjOjdtjwkw32RaeruZlNLVbHDifmQgAIL2AJSFvbZr4kpf7O`;
+      axios.get(`https://api.twitter.com/1.1/trends/place.json?id=1398823`)
+        .then((resp) => {
+          const trending = resp.data[0].trends;
+          const trends = [];
+          trending.forEach(trend => {
+            const newTrend = {
+              title: trend.name.split(" ").join(""),
+            }
+            if (newTrend.title.charAt(0) !== '#') {
+              newTrend.query = newTrend.title.split(/(?=[A-Z])/).join(" ")
+              newTrend.title = `#${newTrend.title}`;
+            } else {
+              newTrend.query = newTrend.title.substr(1).split(/(?=[A-Z])/).join(" ")
+            }
+            trends.push(newTrend);
+          });
+          return res.status(200).json({
+            message: 'success', trends
+          });
+        })
+        .catch((err) => {
+          return res.status(400).json({
+            message: 'error', err: err.message
+          });
+        })
+    }
+  );
+
+  app.post('/api/signup',
+    (req, res) => {
+      const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+      if (!emailRegex.test(req.body.email)) {
         return res.status(400).json({
-          message: 'error', err: err.message
+          message: 'Email is not rightly formatted'
         });
-      })
-  }
-);
+      }
+      User.findOne({
+        email: req.body.email
+      }).then((existingUser) => {
+        if (!existingUser) {
+          const user = new User(req.body);
+          user.save((err, newUser) => {
+            if (err) {
+              return res.status(500).send({ err });
+            }
+            const userDetails = {
+              email: newUser.email,
+              id: newUser._id,
+            };
+            const jsonToken = createToken({ userDetails }, '24h');
+            return res.status(200).send({ message: 'signup successful', jsonToken });
+          });
+        } else {
+          return res.status(400)
+            .send({ message: 'email already exists' });
+        }
+      });
+    }
+  );
+
+  app.post('/api/login',
+    (req, res) => {
+      User.findOne({
+        email: req.body.email
+      }, (err, user) => {
+        if (err) return res.status(500).send({ err });
+        if (
+          !user ||
+          !user.comparePassword(req.body.password)
+        ) {
+          return res.status(404)
+            .send({ message: 'email or password is incorrect' });
+        }
+        const userDetails = {
+          email: user.email,
+          id: user._id,
+        };
+        const jsonToken = createToken({ userDetails }, '24h');
+        return res.status(200).send({ message: 'login successful', jsonToken });
+      });
+    }
+  );
 }
